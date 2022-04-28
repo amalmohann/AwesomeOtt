434fb5d5729d71141a987fb1095fc16f46188d49
amalmohann <amalmohann@gmail.com>
2022-04-29 01:01:33 +0530

    added header content component

---
+++ src/components/content/styles.ts
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  contentImageContainer: {
+    marginBottom: 7,
+    // paddingHorizontal: 24,
+  },
+  contentTitle: {
+    fontSize: 18,
+    color: '#ffffff',
+    fontWeight: 'normal',
+    textAlign: 'left',
+  },
+  content: {
+    marginBottom: 30,
+    marginHorizontal: 4,
+  },
+});
+
+export default styles;
