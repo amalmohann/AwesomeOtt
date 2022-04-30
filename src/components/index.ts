0aee17b4bc5b669e5b21c56b7faf5d6eb36b28cf
amalmohann <amalmohann@gmail.com>
2022-04-30 11:44:15 +0530

    added the components index file

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export {default as ImageIcon} from './common/ImageIcon/ImageIcon';
+export {default as ContentGrid} from './contentGrid/ContentGrid';
+export {default as Header} from './header/Header';
